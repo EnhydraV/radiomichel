d413046cfdab789b588d519043fbd263150113a7
Vincent Vanwaelscappel <vincent@cubedesigners.com>
2024-10-03 08:53:53 +0200

    customise contents for Radio Michel

--- setup_gusi_FR.sh
+++ setup_gusi_FR.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+cd /root/gusi-radio
+git stash
+git pull
+
 echo "Stop MPD service"
 sudo service mpd stop
 
 echo "Moving files"
-sudo cp /root/gusi-radio/8192cu.conf /etc/modprobe.d/
-sudo cp /root/gusi-radio/asound.conf /etc/
-sudo cp /root/gusi-radio/mpd.conf /etc/
-sudo cp /root/gusi-radio/FR/* /var/lib/mpd/music/
-sudo cp /root/gusi-radio/cleanshutd.conf /etc/
-sudo cp /root/gusi-radio/rc.local /etc/
+sudo rm /etc/modprobe.d/8192cu.conf
+sudo ln -s /root/gusi-radio/8192cu.conf /etc/modprobe.d/8192cu.conf
+sudo rm /etc/asound.conf
+sudo ln -s /root/gusi-radio/asound.conf /etc/asound.conf
+sudo rm  /etc/mpd.conf
+sudo cp /root/gusi-radio/mpd.conf /etc/mpd.conf
+sudo rm -rf /var/lib/mpd/music/
+sudo ln -s /root/gusi-radio/FR /var/lib/mpd/music
+sudo rm /etc/cleanshutd.conf
+sudo ln -s /root/gusi-radio/cleanshutd.conf /etc/cleanshutd.conf
+sudo rm /etc/rc.local
+sudo ln -s /root/gusi-radio/rc.local /etc/rc.local
 
 echo "Cleaning up"
 sudo systemctl stop dphys-swapfile
